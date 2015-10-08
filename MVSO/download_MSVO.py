8c67745d3e60caa93fdf80e631a641c912c31756
vcampos <victorcampos.7@gmail.com>
2015-10-08 12:38:48 +0200

    The algorithm is now ready

--- MVSO/download_MSVO.py
+++ MVSO/download_MSVO.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import csv
+import urllib2
+import time
 
 CSV_folder_path = ''
 MVSO_destination_folder = ''
@@ -11,10 +14,24 @@ def create_dir(directory):
         print 'Creating dir: ', directory
 
 
+''' MAIN CODE '''
+if len(sys.argv) >= 3:
+    try:
+        CSV_folder_path = str(sys.argv[1])
+        MVSO_destination_folder = str(sys.argv[2])
+    except:
+        sys.exit('The given arguments are not correct')
+else:
+    sys.exit('Arguments: CSV_folder_path MVSO_destination_folder')
+
+create_dir(MVSO_destination_folder)
+
 for file_name in os.listdir(CSV_folder_path):
     # Check if the file is a CSV file
     if not file_name.__endswith__('.csv'):
         continue
+
+    print '\n\nDownloading images in ' + file_name + '\n'
 
     # Create the language folder
     language = file_name.split('.')[0]
@@ -25,13 +42,28 @@ for file_name in os.listdir(CSV_folder_path):
     csv_file = open(os.path.join(CSV_folder_path, file_name))
 
     # Iterate and download images
-    first_line = True # first line does not contain any link
+    index = 0  # first line does not contain any link
+    failed_images = 0
     for row in csv_file:
-        if first_line:
-            first_line = False
+        if index == 0:
+            index += 1
             continue
         anp, url = row.split(',')
         anp_dir = os.path.join(language_dir, anp, '')
         create_dir(anp_dir)
         file_path = os.path.join(anp_dir, url.split('/')[-1])
-        # Download image (urllib2???)
+        # Download image
+        try:
+            f = urllib2.urlopen(url)
+            with open(file_path, "wb") as downloaded_file:
+                downloaded_file.write(f.read())
+            # Random wait to avoid Flickr banning us
+            time.sleep(0.2)
+            index += 1
+        except:
+            failed_images += 1
+        if index%20 == 0:
+            print '[' + language + '] Images processed: ' + str(index)
+
+    # Close file
+    csv_file.close()
