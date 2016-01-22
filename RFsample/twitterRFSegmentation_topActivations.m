bd699f70f0c863c6ef3407d0f6d8001e13549554
vcampos <victorcampos.7@gmail.com>
2016-01-22 13:17:35 +0100

    Modified version that computes RF for images with the highest activations

---
+++ RFsample/twitterRFSegmentation_topActivations.m
@@ -0,0 +1,47 @@
+%% Parameters
+images_folder = '/imatge/vcampos/work/twitter_dataset/images/';
+features_folder = '/imatge/vcampos/work/twitter_dataset/feature_maps_mat/test1/conv5/';
+ground_truth_file = '/imatge/vcampos/work/twitter_dataset/feature_maps_mat/test1.txt';
+unitID = 49;                    % conv5 unit
+N_images = 5;
+
+
+%% Load text file
+images = textread(ground_truth_file, '%s');
+maxValFeatureMaps = [];
+
+
+%% Load feature maps for unitID and compute their sum
+for i=1:size(images,1)
+    curFeatureMap = load([features_folder cast(images(i),'char') '.mat']); % the extracted feature map for unitID at conv5
+    curFeatureMap = curFeatureMap.featureMap(unitID,:,:);  % select activation from unitID
+    maxValFeatureMaps = [maxValFeatureMaps max(curFeatureMap(:))];
+end
+
+
+%% Sort images so only the ones with the top activations are displayed
+[values, sorted_indexes] = sort(maxValFeatureMaps, 'descending');
+
+fig = figure('visible','off');
+
+
+%% Estimate RF for the images that generated the highest unitID activations
+for j=1:+N_images
+    curImg = imread([images_folder cast(images(images(sorted_indexes(j))),'char') '.jpg']);
+    curImg = im2double(imresize(curImg,para.imageScale));
+    curFeatureMap = load([features_folder cast(images(sorted_indexes(j)),'char') '.mat']); % the extracted feature map for unitID at conv5
+    curFeatureMap = curFeatureMap.featureMap(unitID,:,:);
+    curFeature_vectorized = curFeatureMap(:);
+    maxValue = max(curFeature_vectorized);
+    IDX_max = find(curFeature_vectorized>maxValue * thresholdSegmentation);
+    curMask = squeeze(sum(maskRF(IDX_max,:,:),1));
+    curMask(curMask>0) = 1;
+
+    IDX_region = find(curMask>0);
+    curSegmentation = repmat(curMask,[1 1 3]).*curImg+0.2*(1- repmat(curMask,[1 1 3])).*curImg;
+
+    subplot(1,N_images,j),imshow(curSegmentation);
+end
+
+%% Save figure
+saveas(fig,'/imatge/vcampos/work/fig','jpg');
