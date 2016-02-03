ccc1f39057b150766b24005f01c0123626ac7c9b
victorcampos7 <victorcampos.7@gmail.com>
2016-02-03 19:57:00 +0100

    All accuracies and losses are now plotted

--- caffe_tools/plot_caffe_log.py
+++ caffe_tools/plot_caffe_log.py
@@ -18,82 +18,125 @@ from mpl_toolkits.axes_grid1 import host_subplot
 
 if __name__ == "__main__":
 
-  parser = argparse.ArgumentParser(description='makes a plot from Caffe output')
-  parser.add_argument('output_file', help='file of captured stdout and stderr')
+  parser = argparse.ArgumentParser(description='Makes a plot from Caffe output')
+  parser.add_argument('training_log', help='file of captured stdout and stderr')
+  parser.add_argument('output_path', help='folder where the generated plots will be stored')
+  parser.add_argument('output_files_prefix', help='prefix for the output files')
   args = parser.parse_args()
+
+  output_path = args.output_path
+  if output_path[-1] != '/':
+    output_path = output_path + '/'
   
-  f = open(args.output_file, 'r')
+  f = open(args.training_log, 'r')
 
   training_iterations = []
   training_loss = []
+  training_accuracy = []
 
   test_iterations = []
   test_accuracy = []
   test_loss = []
 
-  check_test = False
-  check_test2 = False
   for line in f:
-
-    if check_test:
-      test_accuracy.append(float(line.strip().split(' = ')[-1]))
-      check_test = False
-      check_test2 = True
-    elif check_test2:
-      if 'Test net output' in line and 'valid_log_loss' in line:
-        #print line
-        #print line.strip().split(' ')
-        test_loss.append(float(line.strip().split(' ')[-2]))
-        check_test2 = False
-      else:
-        test_loss.append(0)
-        check_test2 = False
 
     if '] Iteration ' in line and 'loss = ' in line:
       arr = re.findall(r'ion \b\d+\b,', line)
       training_iterations.append(int(arr[0].strip(',')[4:]))
-      training_loss.append(float(line.strip().split(' = ')[-1]))
+      #training_loss.append(float(line.strip().split(' = ')[-1]))
+      #check_train = True
 
     if '] Iteration ' in line and 'Testing net' in line:
       arr = re.findall(r'ion \b\d+\b,', line)
       test_iterations.append(int(arr[0].strip(',')[4:]))
-      check_test = True
+      #check_test = True
+
+    if 'Test net output' in line and 'loss' in line:
+      test_loss.append(float(line.strip().split(' ')[-2]))
+
+    if 'Test net output' in line and 'accuracy' in line:
+      test_accuracy.append(float(line.strip().split(' ')[-1]))
+
+    if 'Train net output' in line and 'loss' in line:
+      training_loss.append(float(line.strip().split(' ')[-2]))
+
+    if 'Train net output' in line and 'accuracy' in line:
+      training_accuracy.append(float(line.strip().split(' ')[-1]))
 
   print 'train iterations len: ', len(training_iterations)
   print 'train loss len: ', len(training_loss)
+  print 'train accuracy len: ', len(training_accuracy)
   print 'test loss len: ', len(test_loss)
   print 'test iterations len: ', len(test_iterations)
   print 'test accuracy len: ', len(test_accuracy)
 
-  if len(test_iterations) != len(test_accuracy): #awaiting test...
-    print 'mis-match'
-    print len(test_iterations[0:-1])
+  if len(training_iterations) != len(training_accuracy): #awaiting test...
+    print 'mis-match (training)'
+    print len(training_iterations[0:-1])
+    training_iterations = training_iterations[0:-1]
     test_iterations = test_iterations[0:-1]
+    test_accuracy = test_accuracy[0:-1]
+    test_loss = test_loss[0:-1]
+
+  '''if len(test_iterations) != len(test_accuracy): #awaiting test...
+    print 'mis-match (test)'
+    print len(test_iterations[0:-1])
+    test_iterations = test_iterations[0:-1]'''
 
   f.close()
-#  plt.plot(training_iterations, training_loss, '-', linewidth=2)
-#  plt.plot(test_iterations, test_accuracy, '-', linewidth=2)
-#  plt.show()
   
+
+  # Plot loss
   host = host_subplot(111)#, axes_class=AA.Axes)
   plt.subplots_adjust(right=0.75)
 
-  par1 = host.twinx()
+  #par1 = host.twinx()
 
-  host.set_xlabel("iterations")
-  host.set_ylabel("log loss")
-  par1.set_ylabel("validation accuracy")
+  host.set_xlabel("Iterations")
+  host.set_ylabel("Loss")
+  #par1.set_ylabel("validation accuracy")
  
-  p1, = host.plot(training_iterations, training_loss, label="training log loss")
-  p3, = host.plot(test_iterations, test_loss, label="valdation log loss")
-  p2, = par1.plot(test_iterations, test_accuracy, label="validation accuracy")
+  if len(training_loss) > 0:
+    p1, = host.plot(training_iterations, training_loss, label="Train")
+  if len(test_loss) > 0:
+    p3, = host.plot(test_iterations, test_loss, label="Validation")
+  #p2, = par1.plot(test_iterations, test_accuracy, label="validation accuracy")
+  #p4, = par1.plot(training_iterations, training_accuracy, label="train accuracy")
 
-  host.legend(loc=2)
+  host.legend(loc=1)
 
-  host.axis["left"].label.set_color(p1.get_color())
-  par1.axis["right"].label.set_color(p2.get_color())
+  #host.axis["left"].label.set_color(p1.get_color())
+  #par1.axis["right"].label.set_color(p2.get_color())
 
   plt.draw()
   plt.show()
 
-  plt.savefig('plot.png')
+  plt.savefig(output_path + args.output_files_prefix + '_loss.png')
+
+  # Plot accuracy
+  plt.clf()
+  host = host_subplot(111)#, axes_class=AA.Axes)
+  plt.subplots_adjust(right=0.75)
+
+  #par1 = host.twinx()
+
+  host.set_xlabel("Iterations")
+  host.set_ylabel("Accuracy")
+  #par1.set_ylabel("validation accuracy")
+ 
+  if len(training_accuracy) > 0:
+    p1, = host.plot(training_iterations, training_accuracy, label="Train")
+  if len(test_accuracy) > 0:
+    p3, = host.plot(test_iterations, test_accuracy, label="Validation")
+  #p2, = par1.plot(test_iterations, test_accuracy, label="validation accuracy")
+  #p4, = par1.plot(training_iterations, training_accuracy, label="train accuracy")
+
+  host.legend(loc=4)
+
+  #host.axis["left"].label.set_color(p1.get_color())
+  #par1.axis["right"].label.set_color(p2.get_color())
+
+  plt.draw()
+  plt.show()
+
+  plt.savefig(output_path + args.output_files_prefix + '_accuracy.png')
