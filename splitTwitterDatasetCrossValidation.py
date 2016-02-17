4e5ee1305ea6093e438f80677c3af6ae31f75ff3
victorcampos7 <victorcampos.7@gmail.com>
2016-02-17 15:45:29 +0100

    Extended to work with the 3 subsets by changing a parameter

--- splitTwitterDatasetCrossValidation.py
+++ splitTwitterDatasetCrossValidation.py
@@ -3,12 +3,15 @@ import sys
 import random
 import time
 import numpy as np
-import CNN_tools
+import argparse
 
 
 '''
- Splits Twitter "five agrees" dataset for 5-fold cross-validation
- Subsets: 5*176 = 880 images (the full set has 882 images, but this way every subset has the same size)
+ Splits Twitter "X agrees" dataset for 5-fold cross-validation
+ Subsets:
+    five-agree: 5*176 = 880 images (the full set has 882 images, but this way every subset has the same size)
+    four-agree: 5*223 = 1115 images (the full set has 1116 images, but this way every subset has the same size)
+    three-agree: 5*253 = 1265 images (the full set has 1269 images, but this way every subset has the same size)
 '''
 
 
@@ -16,8 +19,39 @@ GROUND_TRUTH_PATH = r"/imatge/vcampos/work/twitter_dataset/ground_truth/" # the 
 IMAGES_PATH = r"/imatge/vcampos/work/twitter_dataset/images/resized/" # the r at the beginning solves some problems with the spaces in the path
 instanceList = []
 fraction = 0.2
-ground_truth_file = "twitter_five_agrees.txt"
-subsetSize = 176
+ground_truth_file = "twitter_four_agrees.txt"
+output_folder = "5-fold_cross-validation_four_agrees/"
+subsetSize = 223
+
+
+def createDir( directory ):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print 'Create dir: ', directory
+    return directory
+
+
+
+parser = argparse.ArgumentParser(description='Splits Twitter x-agree dataset for 5-fold cross-validation')
+parser.add_argument('subset', help='Subset to split (3, 4 or 5)')
+args = parser.parse_args()
+
+agrees = int(args.subset)
+
+if agrees == 3:
+    ground_truth_file = "twitter_three_agrees.txt"
+    output_folder = "5-fold_cross-validation_three_agrees/"
+    subsetSize = 253
+elif agrees == 4:
+    ground_truth_file = "twitter_four_agrees.txt"
+    output_folder = "5-fold_cross-validation_four_agrees/"
+    subsetSize = 223
+elif agrees == 5:
+    ground_truth_file = "twitter_five_agrees.txt"
+    output_folder = "5-fold_cross-validation_five_agrees/"
+    subsetSize = 176
+else:
+    sys.exit("Wrong parameters. Chosse between 3, 4 and 5 - agrees subset")
 
 
 # Open the file
@@ -55,11 +89,11 @@ set5 = instanceList[4*subsetSize:5*subsetSize]
 
 t0 = time.time()
 # Write the training/testing files for each experiment in a different folder
-CNN_tools.createDir(GROUND_TRUTH_PATH+"5-fold_cross-validation/")
+createDir(GROUND_TRUTH_PATH+output_folder)
 
 # Using subset 1 for testing
-dir = GROUND_TRUTH_PATH+"5-fold_cross-validation/"+"test1/"
-CNN_tools.createDir(dir)
+dir = GROUND_TRUTH_PATH+output_folder+"test1/"
+createDir(dir)
 file_train = open(dir+"train.txt", "w")
 for line in set2+set3+set4+set5:
     file_train.write(IMAGES_PATH+line)
@@ -71,8 +105,8 @@ for line in set1:
 file_test.close()
 
 # Using subset 2 for testing
-dir = GROUND_TRUTH_PATH+"5-fold_cross-validation/"+"test2/"
-CNN_tools.createDir(dir)
+dir = GROUND_TRUTH_PATH+output_folder+"test2/"
+createDir(dir)
 file_train = open(dir+"train.txt", "w")
 for line in set1+set3+set4+set5:
     file_train.write(IMAGES_PATH+line)
@@ -84,8 +118,8 @@ for line in set2:
 file_test.close()
 
 # Using subset 3 for testing
-dir = GROUND_TRUTH_PATH+"5-fold_cross-validation/"+"test3/"
-CNN_tools.createDir(dir)
+dir = GROUND_TRUTH_PATH+output_folder+"test3/"
+createDir(dir)
 file_train = open(dir+"train.txt", "w")
 for line in set1+set2+set4+set5:
     file_train.write(IMAGES_PATH+line)
@@ -97,8 +131,8 @@ for line in set3:
 file_test.close()
 
 # Using subset 4 for testing
-dir = GROUND_TRUTH_PATH+"5-fold_cross-validation/"+"test4/"
-CNN_tools.createDir(dir)
+dir = GROUND_TRUTH_PATH+output_folder+"test4/"
+createDir(dir)
 file_train = open(dir+"train.txt", "w")
 for line in set1+set2+set3+set5:
     file_train.write(IMAGES_PATH+line)
@@ -110,8 +144,8 @@ for line in set4:
 file_test.close()
 
 # Using subset 5 for testing
-dir = GROUND_TRUTH_PATH+"5-fold_cross-validation/"+"test5/"
-CNN_tools.createDir(dir)
+dir = GROUND_TRUTH_PATH+output_folder+"test5/"
+createDir(dir)
 file_train = open(dir+"train.txt", "w")
 for line in set1+set2+set3+set4:
     file_train.write(IMAGES_PATH+line)
